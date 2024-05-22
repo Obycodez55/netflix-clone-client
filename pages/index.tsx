19352e3060878657c31103d552fad5f9e384c27e
Obikoya Adebayo <obikoyaadebayo55@gmail.com>
2024-05-22 15:36:49 +0100

    cleaned up homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -31,23 +31,10 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
-  const { data: user} = useCurrentUser();
-  const {push} = useRouter();
-  const logout = useCallback(async()=>{
-    try {
-    await axios.delete(`api/logout`);
-      push("/auth");
-  } catch (error:unknown) {
-      console.log(error);
-}}, [push])
 
   return (
     <>
-    <h1 className="text-4xl text-green-500">Netflix clone</h1>
-    <p className="text-white">Logged In as : {user?.username}</p>
-    <button className="h-10 w-full bg-white hover:bg-neutral-400" onClick={logout}>
-      Logout!
-    </button>
+   
     </>
   );
 }
