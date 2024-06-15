d0df07e91f69764b9bed8dbddad1c1db9091b7f4
Obikoya Adebayo <128065847+Obycodez55@users.noreply.github.com>
2024-06-15 14:02:25 +0000

    added remote server url

--- Utils.ts
+++ Utils.ts
@@ -1,3 +1,4 @@
-const baseUrl = "http://localhost:8080/api/v1"
+const baseUrl1 = "http://localhost:8080/api/v1"
+const baseUrl = "https://netflix-l8v5.onrender.com/api/v1"
 
 export default baseUrl;
