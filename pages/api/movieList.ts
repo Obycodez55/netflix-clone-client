c061c8b72b790ffa5bf22560acfb990e88554137
Obikoya Adebayo <128065847+Obycodez55@users.noreply.github.com>
2024-06-15 14:56:05 +0000

    Added movie list hook

---
+++ pages/api/movieList.ts
@@ -0,0 +1,21 @@
+import baseUrl from "@/Utils";
+import axios from "axios";
+import { NextApiRequest, NextApiResponse } from "next";
+
+
+export default async function handler(request: NextApiRequest, response: NextApiResponse){
+    if (request.method !== "GET") return response.status(405).end();
+    try {
+        const token = request.cookies.token;
+        const {data} = await axios.get(`${baseUrl}/movies/`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+       return response.status(200).send(data);
+    } catch (error) {
+        console.log(error);
+        return response.status(500).end();
+    }
+   
+}
