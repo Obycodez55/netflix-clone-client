c47729a0a99133c301432c9810aacac5beb67dca
Obikoya Adebayo <128065847+Obycodez55@users.noreply.github.com>
2024-06-15 21:03:54 +0000

    Added basic feature for movie list

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,40 +1,53 @@
 import axios from "axios";
-import {parse} from "cookie";
+import { parse } from "cookie";
 import { IncomingMessage } from "http";
 import { NextPageContext } from "next";
 
 import Navbar from "@/components/Navbar";
 import Billboard from "@/components/Billboard";
+import MovieList from "@/components/MovieList";
+import useMovieList from "@/hooks/useMovieList";
 
 export async function getServerSideProps(context: NextPageContext) {
-  const req  = context.req as IncomingMessage;
-  const token = req.headers.cookie? parse(req.headers.cookie).token : undefined;
-  if(!token) return {
-        redirect: {
-          destination: "/auth",
-          permanent: false
-        }
+  const req = context.req as IncomingMessage;
+  const token = req.headers.cookie
+    ? parse(req.headers.cookie).token
+    : undefined;
+  if (!token)
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false
       }
-      const profile = req.headers.cookie? parse(req.headers.cookie).profile : undefined;
-      if(!profile) return {
-        redirect: {
-          destination: "/profiles",
-          permanent: false
-        }
+    };
+  const profile = req.headers.cookie
+    ? parse(req.headers.cookie).profile
+    : undefined;
+  if (!profile)
+    return {
+      redirect: {
+        destination: "/profiles",
+        permanent: false
       }
+    };
   return {
     props: {
       token
     }
-  }
+  };
 }
 
 export default function Home() {
+  const { movies } = useMovieList();
+
 
   return (
     <>
-   <Navbar />
-   <Billboard />
+      <Navbar />
+      <Billboard />
+      <div className="pb-40">
+        <MovieList title="Trending Now" data={movies}/>
+      </div>
     </>
   );
 }
