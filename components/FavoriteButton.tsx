b9c514452e27bd38ebf4dae599373f75124411f0
Obikoya Adebayo <128065847+Obycodez55@users.noreply.github.com>
2024-07-07 22:16:13 +0000

    chore: Refactor FavoriteButton component to use axios for API requests

--- components/FavoriteButton.tsx
+++ components/FavoriteButton.tsx
@@ -1,15 +1,30 @@
-import React from 'react'
+import React, { useCallback } from "react";
 import { useProfile } from "@/contexts/ProfileContext";
-import { AiOutlinePlus } from 'react-icons/ai'
+import { AiOutlineCheck, AiOutlinePlus } from "react-icons/ai";
+import axios from "axios";
 
 type FavoriteButtonProps = {
-    movieId: string
-}
+  movieId: string;
+};
 
-const FavoriteButton: React.FC<FavoriteButtonProps> = ({movieId}) => {
-  const {profile, updateProfile} = useProfile();
+const FavoriteButton: React.FC<FavoriteButtonProps> = ({ movieId }) => {
+  const { profile, updateProfile } = useProfile()!;
+  const isFavourite = profile?.favouriteIds?.includes(movieId);
+
+  const toggleFavourite = useCallback(async () => {
+    if (isFavourite) {
+      await axios.delete("api/favourites", { data: { movieId } });
+    } else {
+      await axios.post("api/favourites", { movieId });
+    }
+    updateProfile();
+  }, [updateProfile, isFavourite, movieId]);
+
+  const Icon = isFavourite ? AiOutlineCheck : AiOutlinePlus;
   return (
-    <div className="
+    <div
+      onClick={toggleFavourite}
+      className="
         cursor-pointer 
         group/item 
         w-6 h-6
@@ -23,10 +38,11 @@ const FavoriteButton: React.FC<FavoriteButtonProps> = ({movieId}) => {
         items-center
         transition
         hover:border-neutral-300
-    ">
-        <AiOutlinePlus size={25} className="text-white"/>
+    "
+    >
+      <Icon size={25} className="text-white" />
     </div>
-  )
-}
+  );
+};
 
-export default FavoriteButton
+export default FavoriteButton;
