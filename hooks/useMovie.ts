4d7a4d3130184b7e1b580cd440a535276fb578c4
Obikoya Adebayo <128065847+Obycodez55@users.noreply.github.com>
2024-07-08 00:32:25 +0000

    chore: Update useMovie hook to fetch movie data from correct API endpoint

--- hooks/useMovie.ts
+++ hooks/useMovie.ts
@@ -3,11 +3,7 @@ import useSWR from "swr";
 import { Movie } from "..";
 
 const useMovie= (id? : string) => {
-    let {data, error, isLoading, mutate} = useSWR(id? `api/currentProfile${id}` : null, fetcher, {
-        revalidateOnFocus: false,
-        revalidateIfStale: false,
-        revalidateOnMount: false
-    });
+    let {data, error, isLoading, mutate} = useSWR(id? `../api/movies/${id}` : null, fetcher);
     const movie = data as Movie;
     return {
         movie,
