b2b4134a59b1d68788c217d5d4b7969f21556c64
Obikoya Adebayo <obikoyaadebayo55@gmail.com>
2024-08-09 05:33:29 +0100

    Update Utils.ts

--- Utils.ts
+++ Utils.ts
@@ -1,4 +1,4 @@
-const baseUrl = "https://4rxwv48p-8080.uks1.devtunnels.ms/api/v1"
-const baseUrl1 = "https://netflix-api-ta4d.onrender.com/api/v1"
+const baseUrl1 = "https://4rxwv48p-8080.uks1.devtunnels.ms/api/v1"
+const baseUrl = "https://netflix-api-ta4d.onrender.com/api/v1"
 
 export default baseUrl;
